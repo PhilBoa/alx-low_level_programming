40fe51378bc4a04a2868ae6aedc80aafc4212480
PhilBoa <philicbrefo@gmail.com>
2023-03-06 11:11:31 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer
+ * @c: character
+ * Return: Always 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+		int counter;
+
+		for (counter = 0; s[counter] != '\0'; counter++)
+		{
+		if (s[counter] == c)
+		{
+		return (s + counter);
+		break;
+		}
+		}
+return ('\0');
+}
