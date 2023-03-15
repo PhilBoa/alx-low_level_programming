e56b74bf2721c0c73570cc9604243cb3d2b7b07d
PhilBoa <philicbrefo@gmail.com>
2023-03-14 17:42:59 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,7 @@
  * @s1: the first string to concatenate
  * @s2: the second string to concatenate
  *
- * Return: the two string to concatenated
+ * Return: the two strings to concatenated
  */
 
 char *str_concat(char *s1, char *s2)
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	s1 = "";
 	if (s2 == NULL)
 	s2 = "";
-	while (s2[i])
+	while (s1[i])
 	i++;
 	while (s2[j])
 	j++;
