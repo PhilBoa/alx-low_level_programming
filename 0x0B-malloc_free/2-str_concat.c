ff2c8b0f7b180198cc3484026599bd580319b77e
PhilBoa <philicbrefo@gmail.com>
2023-03-14 17:33:23 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,33 +12,33 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int a = 0, b = 0, c = 0, d = 0;
+	int i = 0, j = 0, k = 0, l = 0;
 	char *s;
 
 	if (s1 == NULL)
 	s1 = "";
 	if (s2 == NULL)
 	s2 = "";
-	while (s2[b])
-	a++;
-	while (s2[b])
-	b++;
-	d = a + b;
-	s = malloc((sizeof(char) * d) + 1);
+	while (s2[i])
+	i++;
+	while (s2[j])
+	j++;
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
 	if (s == NULL)
 	return (NULL);
-	b = 0;
-	while (c < d)
+	j = 0;
+	while (k < l)
 	{
-	if (c <= a)
-	s[c] = s1[b];
-	if (c >= a)
+	if (k <= i)
+	s[k] = s1[k];
+	if (k >= i)
 	{
-	s[c] = s2[b];
-	b++;
+	s[k] = s2[j];
+	j++;
 	}
-	c++;
+	k++;
 	}
-	s[c] = '\0';
+	s[k] = '\0';
 	return (s);
 }
